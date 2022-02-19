a431ef78295283ea6919ae3a37c60f5ac58959f5
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-02-19 11:36:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './global.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>MERN Tinder Clone</h1>
+      {/* Header */}
+      {/* TinderCars */}
+      {/* SwipeButtons */}
+    </div>
+  )
+}
+
+export default App
